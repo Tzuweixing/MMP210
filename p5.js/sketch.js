cc1cb12c98e606dc145e45a4190b1365e70b51dd
Tzuweixing <44472436+Tzuweixing@users.noreply.github.com>
2019-02-06 14:11:00 -0500

    Update sketch.js

--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -9,33 +9,33 @@ function setup() {
     stroke(255);
     
     var radius = min(width, height) / 2;
-    secondsRadius = radius * 0.61;
-    minutesRadius = radius * 0.60;
+    secondsRadius = radius * 0.61;//change the situation and length of points and second hand
+    minutesRadius = radius * 0.60;//change the length of minute hand
     hoursRadius = radius * 0.30;
-    clockDiameter = radius * 1.7;
+    clockDiameter = radius * 1.7;//change the size of circle
     
     cx = width / 2;
     cy= height / 2;
 }
 
 function draw() {
-  background(250);
+  background(250);//change the color of background
   
   // Draw the clock background
   noStroke();
-  fill(248,158,158);
+  fill(248,158,158);//change the color of excircle
   ellipse(cx, cy, clockDiameter + 25, clockDiameter + 25);
-  fill(27,34,93);
+  fill(27,34,93);//change the color of inner ciircle
   ellipse(cx, cy, clockDiameter, clockDiameter);
   
     // Angles for sin() and cos() start at 3 o'clock;
   // subtract HALF_PI to make them start at the top
-  var s = map(second(), 0, 10, 0, TWO_PI) - HALF_PI;
-  var m = map(minute() + norm(second(), 0, 20), 0, 30, 0, TWO_PI) - HALF_PI; 
+  var s = map(second(), 0, 10, 0, TWO_PI) - HALF_PI;//change the speed of second hand
+  var m = map(minute() + norm(second(), 0, 20), 0, 30, 0, TWO_PI) - HALF_PI; //change the speed of minute hand
   var h = map(hour() + norm(minute(), 0, 60), 0, 10, 0, TWO_PI * 2) - HALF_PI;
   
   // Draw the hands of the clock
-  stroke(355);
+  stroke(355); //change color of inside
   strokeWeight(5);
   line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
   strokeWeight(2);
